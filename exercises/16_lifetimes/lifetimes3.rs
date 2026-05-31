8590f10a72f31b403185b3b1020b7a652e27f874
Mokss <maksim.shuvaev@botmother.com>
2026-05-31 21:19:06 +0300

    31.05.2026

--- exercises/16_lifetimes/lifetimes3.rs
+++ exercises/16_lifetimes/lifetimes3.rs
@@ -1,15 +1,17 @@
 // Lifetimes are also needed when structs hold references.
 
 // TODO: Fix the compiler errors about the struct.
-struct Book {
-    author: &str,
-    title: &str,
+struct Book <'b, T>{
+    author: &'b str,
+    title: &'b str,
+    some: T
 }
 
 fn main() {
-    let book = Book {
+    let book: Book<'_, i32> = Book {
         author: "George Orwell",
         title: "1984",
+        some: 123
     };
 
     println!("{} by {}", book.title, book.author);
